8cdfa4750d5abc3342cbfa3d38f87675b827fad5
tenomo <br.vladis.dev@gmail.com>
2017-08-10 18:27:41 +0300

    fix

--- AbstractQueue/Infrastructure/Singleton.cs
+++ AbstractQueue/Infrastructure/Singleton.cs
@@ -1,37 +1,49 @@
-﻿using System;
-using System.Reflection;
+﻿//using System;
+//using System.Reflection;
 
-namespace AbstractQueue.Infrastructure
-{
-    /// generic Singleton<T> (потокобезопасный с использованием generic-класса и с отложенной инициализацией)
+//namespace AbstractQueue.Infrastructure
+//{
+//    /// generic Singleton<T> (потокобезопасный с использованием generic-класса и с отложенной инициализацией)
 
-    /// <typeparam name="T">Singleton class</typeparam>
-    public class Singleton<T> where T : class
-    {
-        /// Защищённый конструктор необходим для того, чтобы предотвратить создание экземпляра класса Singleton. 
-        /// Он будет вызван из закрытого конструктора наследственного класса.
-        protected Singleton() { }
+//    /// <typeparam name="T">Singleton class</typeparam>
+//    public class Singleton<T> where T : class
+//    {
+//        /// Защищённый конструктор необходим для того, чтобы предотвратить создание экземпляра класса Singleton. 
+//        /// Он будет вызван из закрытого конструктора наследственного класса.
+//        protected Singleton() { Logger.Log(typeof(T).ToString());  }
 
-        /// Фабрика используется для отложенной инициализации экземпляра класса
-        private sealed class SingletonCreator<S> where S : class
-        {
-            //Используется Reflection для создания экземпляра класса без публичного конструктора
-            private static readonly S instance = (S)typeof(S).GetConstructor(
-                        BindingFlags.Instance | BindingFlags.NonPublic,
-                        null,
-                        new Type[0],
-                        new ParameterModifier[0]).Invoke(null);
+//        /// Фабрика используется для отложенной инициализации экземпляра класса
+//        private sealed class SingletonCreator<S> where S : class
+//        {
+//            //Используется Reflection для создания экземпляра класса без публичного конструктора
+//            private static readonly S instance = (S)typeof(S).GetConstructor(
+//                        BindingFlags.Instance | BindingFlags.NonPublic ,
+//                        null,
+//                        new Type[0],
+//                        new ParameterModifier[0]).Invoke(null
+//                );
 
-            public static S CreatorInstance
-            {
-                get { return instance; }
-            }
-        }
+//            public static S CreatorInstance
+//            {
+//                get
+//                {
+                    
+//                    return instance;
+//                }
+//            }
+//        }
 
-        public static T Kernal
-        {
-            get { return SingletonCreator<T>.CreatorInstance; }
-        }
+//        public static T Kernal
+//        {
+//            get
+//            {
+                
+//                return SingletonCreator<T>.CreatorInstance;
+//            }
+//        }
 
-    }
-}
+//    }
+
+    
+
+//}
--- AbstractQueue/Infrastructure/TaskExecutionObserver.cs
+++ AbstractQueue/Infrastructure/TaskExecutionObserver.cs
@@ -1,27 +1,46 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using AbstractQueue.QueueData.Entities;
 using AbstractQueue.TaskStore;
 
 namespace AbstractQueue.Infrastructure
-{
-  internal  class TaskExecutionObserver: Singleton<TaskExecutionObserver>,ITaskExecutionObserver
+{    
+  public   class TaskExecutionObserver : ITaskExecutionObserver // Singleton<TaskExecutionObserver>,ITaskExecutionObserver
     {
+ 
+      //  [assembly: InternalsVisibleTo("AbstractQueueUnitTests")]
+ 
+
         public event Action<ITaskStore, QueueTask  > SuccessExecuteTaskEvent;
         public event Action<ITaskStore, QueueTask> FailedExecuteTaskEvent;
         public event Action<ITaskStore, QueueTask> InProccesTaskEvent;
 
+        
+
+        protected TaskExecutionObserver() { }
+
+        private sealed class TaskExecutionObserverCreator
+        {
+            private static readonly TaskExecutionObserver instance = new TaskExecutionObserver();
+            public static TaskExecutionObserver Instance { get { return instance; } }
+        }
+
+        public static TaskExecutionObserver Kernal
+        {
+            get { return TaskExecutionObserverCreator.Instance; }
+        }
 
         internal void OnSuccessExecuteTaskEvent(ITaskStore obj , QueueTask e )
         {
             SuccessExecuteTaskEvent?.Invoke(obj,e);
         }
 
-        internal virtual void OnFailedExecuteTaskEvent(ITaskStore obj, QueueTask e)
+        internal void OnFailedExecuteTaskEvent(ITaskStore obj, QueueTask e)
         {
             FailedExecuteTaskEvent?.Invoke(obj, e);
         }
 
-        internal virtual void OnInProccesTaskEvent(ITaskStore obj, QueueTask e)
+        internal void OnInProccesTaskEvent(ITaskStore obj, QueueTask e)
         {
             InProccesTaskEvent?.Invoke(obj, e);
         }
